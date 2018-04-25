da8fc128b056135a104375e39e91abac7b54dab5
CapricornWolf <1069345726@qq.com>
2018-04-25 21:51:23 +0800

    4

--- 5. Longest Palindromic Substring/Longest Palindromic Substring.js
+++ 5. Longest Palindromic Substring/Longest Palindromic Substring.js
@@ -3,5 +3,21 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-
+    let res = ''
+    let tempres = ''
+    let temp = ''
+    for(let i = 0; i < s.length; i ++){
+        temp = s.substring(i,s.length).split('').reverse().join('')
+        if (temp.indexOf(s[i]) > -1){
+            tempres = s.substring(i,s.length - temp.indexOf(s[i]))
+        }
+        if (tempres.length > res.length){
+            res = tempres
+        }
+    }
+    return res
 };
+
+console.log(longestPalindrome('babad'))
+console.log(longestPalindrome('cbbd'))
+console.log(longestPalindrome("abcda"))
